b2ae06e706a4c96bc688cf12c5b759dea519e477
Celso Passos <146108688+CelsoPassosdev@users.noreply.github.com>
2024-08-20 19:36:00 -0300

    desafio modulo 1

---
+++ md1.js
@@ -0,0 +1,33 @@
+//Desafio modulo 1
+let nomeHeroi = [ "Rambo" , "John Wick" , "Jackie Chan" , "Homen-Aranha" , "007" , "Batiman"]
+// matriz nivel de experiência do heroi
+let experienciaXP = [
+  [10000],
+  [9500],
+  [8500],
+  [8000],
+  [6000],
+  [5000],
+    
+] 
+//const 
+//if
+
+//niveis de experiencia
+//let pontosXP = [
+console.log("Se XP for menor do que 1.000 = Ferro")
+console.log("Se XP for entre 1.001 e 2.000 = Bronze") 
+console.log("Se XP for entre 2.001 e 5.000 = Prata", "Batiman"),
+console.log("Se XP for entre 5.001 e 7.000 = Ouro", "007"),
+console.log("Se XP for entre 7.001 e 8.000 = Platina"), "Homen-Aranha",
+console.log("Se XP for entre 8.001 e 9.000 = Ascendente"), "Jackie Chan",
+console.log("Se XP for entre 9.001 e 10.000 = Imortal"), "John Wick",
+console.log("Se XP for maior ou igual a 10.001 = Radiante")
+//]
+//let nomeNivel = ("Ferro" , "Bronze" , "Prata" , "Ouro" , "Platina" , "Ascendente" , "Imortal" , "Radiante")
+//input para perguntar o nome do heroi e a quantidade de XP que possui
+//do {}
+//while  
+//armazenar resposta das duas perguntas
+
+console.log("O Herói de nome " + nomeHeroi[1],"está no nível de XP " + experienciaXP[1])
